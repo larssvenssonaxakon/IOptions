2d578251e38d453a78fda03132512697e8d2dc74
Lars Svensson <lars.svensson@axakon.se>
2021-10-21 22:14:50 +0200

    add v2 swagger doc

--- MinimalApi/Program.cs
+++ MinimalApi/Program.cs
@@ -19,6 +19,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new() { Title = "TestNet6", Version = "v1" });
+    c.SwaggerDoc("v2", new() { Title = "TestNet6", Version = "v2" });
 });
 
 await using var app = builder.Build();
@@ -31,12 +32,13 @@ if (app.Environment.IsDevelopment())
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI(c => {
-        c.SwaggerEndpoint("/swagger/v1/swagger.json", "TestNet6 v1");
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "v1");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "v2");
         c.RoutePrefix = string.Empty;
         });
 }
 
-app.MapGet("/hello/{name}", (string name, string thisIsFromQuery) =>
+app.MapGet("v1/hello/{name}", (string name, string thisIsFromQuery) =>
 {
     if (thisIsFromQuery == null)
     {
@@ -47,11 +49,14 @@ app.MapGet("/hello/{name}", (string name, string thisIsFromQuery) =>
     return Results.Ok(result);
 }).WithName("get-hello") //Swagger operation-id
 .WithTags("HelloGroup") //Grouping in swagger, doesn't seem to work
+.WithGroupName("v1")
 .Produces(400).Produces<string>(200); //Describe result along with http status code
 
-app.MapPost("/hello/{name}", (string name, string thisIsFromQuery, TestRequest testRequest) => $"Hello from route {name}! Hello from query {thisIsFromQuery}! Hello from body {testRequest.Body}");
+app.MapPost("v2/hello/{name}", (string name, string thisIsFromQuery, TestRequest testRequest) => $"Hello from route {name}! Hello from query {thisIsFromQuery}! Hello from body {testRequest.Body}")
+    .WithGroupName("v2");
 
-app.MapGet("/handler/{name}", (string name, [FromServices] ITestHandler handler) => handler.Handle(name));
+app.MapGet("v1/handler/{name}", (string name, [FromServices] ITestHandler handler) => handler.Handle(name))
+    .WithGroupName("v1");
 
 app.MapGet("/noshow", () => { })
     .ExcludeFromDescription(); //Hide from swagger.
